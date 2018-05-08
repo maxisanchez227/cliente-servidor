969adf566214c9dd2442cf1d469d061e0b6be466
maximilianosanchez <maxisanchez227@gmail.com>
2018-05-07 23:37:07 -0300

    persistencia aspnet

---
+++ tp1/tp1/Repositories/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using tp1.Models;
+
+namespace tp1.Repositories
+{
+    public class UnitOfWork
+    {
+        public UnitOfWork()
+        {
+            this.context = new MyDbContext();
+        }
+        private readonly MyDbContext context;
+
+        private GenericRepository<Post> postsRepository;
+        public GenericRepository<Post> PostsRepository
+        {
+            get
+            {
+                if (this.postsRepository == null)
+                {
+                    this.postsRepository = new GenericRepository<Post>(this.context);
+                }
+                return this.postsRepository;
+            }
+        }
+
+        private GenericRepository<User> usersRepository;
+        public GenericRepository<User> UsersRepository
+        {
+            get
+            {
+                if (this.usersRepository == null)
+                {
+                    this.usersRepository = new GenericRepository<User>(this.context);
+                }
+                return this.usersRepository;
+            }
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
